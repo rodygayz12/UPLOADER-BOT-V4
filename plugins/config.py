0230c59bd53008a5608cc217c81cf2ff8567af34
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-08-28 11:59:25 +0400

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,23 +11,25 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 7075227623:AAEW87tuV5TJ_O0IJ3awIsCtWxOUomvlDro")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "21503867"))
     
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "e0e50d95e1319072731fb0a4a585e9d5")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
     MAX_FILE_SIZE = 2097152000
     
     TG_MAX_FILE_SIZE = 2097152000
+
+    #PORT = int(os.environ.get('PORT', 5000))
     
     FREE_USER_MAX_FILE_SIZE = 2097152000
     
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     
@@ -39,19 +41,20 @@ class Config(object):
     
     DEF_WATER_MARK_FILE = "UploadLinkToFileBot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://onfarooq2:onfarooq2@cluster0.ax3qpvn.mongodb.net/?retryWrites=true&w=majority")
     
     SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002083195253"))
     
     LOGGER = logging
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002035523434")
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6221136155"))
     
     TG_MIN_FILE_SIZE = 2097152000
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-                                  
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "UrlUploaderWorkbot")
+
+    
